ff9db9a7446f20356f3b42f6a87f0c90fbd56ecf
hxh1234 <anas.masaaf@gmail.com>
2015-12-02 22:00:18 +0000

    22:00 02/12/15 anas

--- src/main/java/com/lambda/bilan/domain/PlanAmelioration.java
+++ src/main/java/com/lambda/bilan/domain/PlanAmelioration.java
@@ -1,7 +1,33 @@
 package com.lambda.bilan.domain;
 
+import java.util.List;
+
+import com.lambda.bilan.entities.Action;
+import com.lambda.bilan.entities.PlanFormation;
+
 public class PlanAmelioration {
 	
+	private List<PlanFormation> planFormations; 
+	private List<Action> actions;
+	
+	public PlanAmelioration(List<PlanFormation> planFormations, List<Action> actions) {
+		super();
+		this.planFormations = planFormations;
+		this.actions = actions;
+	}
+	
+	public List<PlanFormation> getPlanFormations() {
+		return planFormations;
+	}
+	public void setPlanFormations(List<PlanFormation> planFormations) {
+		this.planFormations = planFormations;
+	}
+	public List<Action> getActions() {
+		return actions;
+	}
+	public void setActions(List<Action> actions) {
+		this.actions = actions;
+	}
 	
 	
 	
