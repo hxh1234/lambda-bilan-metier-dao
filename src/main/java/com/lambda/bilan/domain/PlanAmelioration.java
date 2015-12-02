3d55cfa736e0f3e2c24870cf61f59e74ed59c3f2
hxh1234 <anas.masaaf@gmail.com>
2015-12-02 23:22:51 +0000

    Domain Package Anas 23:22 02/12/2015

--- src/main/java/com/lambda/bilan/domain/PlanAmelioration.java
+++ src/main/java/com/lambda/bilan/domain/PlanAmelioration.java
@@ -2,32 +2,39 @@ package com.lambda.bilan.domain;
 
 import java.util.List;
 
+import com.lambda.bilan.domain.helpers.PlanFormationRevised;
 import com.lambda.bilan.entities.Action;
-import com.lambda.bilan.entities.PlanFormation;
 
 public class PlanAmelioration {
 	
-	private List<PlanFormation> planFormations; 
+	private List<PlanFormationRevised> planFormations; 
 	private List<Action> actions;
 	
-	public PlanAmelioration(List<PlanFormation> planFormations, List<Action> actions) {
+	public PlanAmelioration(List<PlanFormationRevised> planFormations, List<Action> actions) {
 		super();
 		this.planFormations = planFormations;
 		this.actions = actions;
 	}
-	
-	public List<PlanFormation> getPlanFormations() {
+
+	public List<PlanFormationRevised> getPlanFormations() {
 		return planFormations;
 	}
-	public void setPlanFormations(List<PlanFormation> planFormations) {
+
+	public void setPlanFormations(List<PlanFormationRevised> planFormations) {
 		this.planFormations = planFormations;
 	}
+
 	public List<Action> getActions() {
 		return actions;
 	}
+
 	public void setActions(List<Action> actions) {
 		this.actions = actions;
 	}
+	
+	
+	
+	
 	
 	
 	
---
+++ src/main/java/com/lambda/bilan/domain/helpers/PlanFormationRevised.java
@@ -0,0 +1,55 @@
+package com.lambda.bilan.domain.helpers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.lambda.bilan.entities.Objectif;
+import com.lambda.bilan.entities.PlanFormation;
+
+public class PlanFormationRevised {
+
+	private Integer dureePlanFormation;
+	private String moyensPlanFormation;
+	private List<Map<String, Object>> objectifs;
+
+	private PlanFormationRevised(){}
+	public PlanFormationRevised(PlanFormation planFormation) {
+		if(planFormation!=null){
+			this.dureePlanFormation=planFormation.getDureePlanFormation();
+			this.moyensPlanFormation=planFormation.getMoyensPlanFormation();
+			for(Objectif objectif : planFormation.getObjectifs()){
+				objectifs.add(getMapForObjectif(objectif));
+			}
+		}
+	}
+
+	private Map<String, Object> getMapForObjectif(Objectif objectif){
+		if(objectif==null)
+			return null;
+
+		Map<String, Object> hash = new HashMap<String, Object>();		
+		hash.put("idObjectif", objectif.getIdObjectif());
+		hash.put("dateCreationObjectif", objectif.getDateCreationObjectif());
+		hash.put("nomObjectif", objectif.getNomObjectif());
+		hash.put("descriptifObjectif", objectif.getDescriptifObjectif());
+		hash.put("statutObjectif", objectif.getStatutObjectif());
+		return hash;
+	}
+
+	public Integer getDureePlanFormation() {
+		return dureePlanFormation;
+	}
+	public String getMoyensPlanFormation() {
+		return moyensPlanFormation;
+	}
+	public List<Map<String, Object>> getObjectifs() {
+		return objectifs;
+	}
+
+
+
+
+
+
+}
