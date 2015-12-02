e25eb6862536d021a2f4a25ce2f7fb2193d06c81
hxh1234 <anas.masaaf@gmail.com>
2015-12-02 02:03:45 +0000

    commit STL

--- src/main/java/com/lambda/bilan/boot/Boot.java
+++ src/main/java/com/lambda/bilan/boot/Boot.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
